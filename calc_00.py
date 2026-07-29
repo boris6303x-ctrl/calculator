4742226c10a2a316ed5c240514dce94ed0454b20
Boris12345 <boris6303x@gmail.com>
2026-07-29 13:28:33 +0300

    The first version of the calculator

---
+++ calc_00.py
@@ -0,0 +1,30 @@
+print("Calculator 1.0")
+# simple calculator for my IT lessons
+
+x = input("Enter 1st number: ")
+y = input("Enter 2nd number: ")
+result_str = "Result: "
+
+print("Function guide: 1 = Addition(+), 2 = Subtraction(-), 3 = Multiplication(*), 4 = Division(/)")
+f = int(input("Enter function Nr.: "))
+
+if (x.isdigit() and y.isdigit()):
+    x = int(x)
+    y = int(y)
+    if f == 1:
+        print(result_str, x + y)
+    elif f == 2:
+        print(result_str, x - y)
+    elif f == 3:
+        print(result_str, x * y)
+    elif f == 4:
+        if y == 0:
+            print("ZeroDivisionError: can't divide by 0")
+        elif x / y == int(x / y):
+            print(result_str, int(x / y))
+        else:
+            print(result_str, x / y)
+    else:
+        print("Error: Invalid function number")
+else:
+    print("TypeError: Invalid input")
